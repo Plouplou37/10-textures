9450842e2ef4481cbb000d91d4663feae53eb734
Plouplou37 <quentin.plourdeau@gmail.com>
2024-01-24 16:46:14 +0800

    end of lesson

--- src/script.js
+++ src/script.js
@@ -1,5 +1,77 @@
+import './style.css'
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import GUI from 'lil-gui'
+
+const gui = new GUI({
+    title: 'Texture Lesson Debugger',
+    width: 400,
+    closeFolders: true
+});
+gui.close()
+
+/**
+ * Textures
+ */
+// const image = new Image()
+// const texture = new THREE.Texture(image)
+// texture.colorSpace = THREE.SRGBColorSpace
+
+// image.onload = () =>
+// {
+//     texture.needsUpdate = true
+// }
+
+// image.src = '/textures/door/color.jpg'
+
+//We can use the loadingManager to mutualize the events. It's useful if we want to know the global loading progress informed when everything is loading
+const loadingManager = new THREE.LoadingManager()
+loadingManager.onStart = ()=>
+{
+    console.log('On Start')
+},
+loadingManager.onLoad = ()=>
+{
+    console.log('On Load')
+}
+loadingManager.onError = ()=>
+{
+    console.log('On Error')
+}
+
+//One texture loader can load multiple texture. no need to instantiate one for each texture
+const textureLoader = new THREE.TextureLoader(loadingManager)
+const colorTexture = textureLoader.load('/textures/minecraft.png')
+const alphaTexture = textureLoader.load('/textures/door/alpha.jpg')
+const heightTexture = textureLoader.load('/textures/door/height.jpg')
+const metalnessTexture = textureLoader.load('/textures/door/metalness.jpg')
+const normalTexture = textureLoader.load('/textures/door/normal.jpg')
+const roughnessTexture = textureLoader.load('/textures/door/roughness.jpg')
+const ambientOcclusionTexture = textureLoader.load('/textures/door/ambientOcclusion.jpg')
+
+colorTexture.colorSpace = THREE.SRGBColorSpace
+// colorTexture.repeat.x = 2
+// colorTexture.repeat.y = 3
+// colorTexture.wrapS = THREE.MirroredRepeatWrapping
+// colorTexture.wrapT = THREE.MirroredRepeatWrapping
+
+// colorTexture.offset.x = 0.5
+// colorTexture.offset.y = 0.5
+// colorTexture.rotation = -1 * Math.PI/4
+// colorTexture.center.x = 0.5
+// colorTexture.center.y = 0.5
+
+//Filter to make the texture more blurry
+//colorTexture.minFilter = THREE.NearestFilter
+//Filter to make the texture better less blurry
+
+// when using nearestFilter no need mipmap so faster and more performance !
+colorTexture.generateMipmaps = false
+colorTexture.magFilter = THREE.NearestFilter
+
+
+const axesHelper = new THREE.AxesHelper(50)
+
 
 /**
  * Base
@@ -9,12 +81,18 @@ const canvas = document.querySelector('canvas.webgl')
 
 // Scene
 const scene = new THREE.Scene()
+scene.add(axesHelper)
 
 /**
  * Object
  */
 const geometry = new THREE.BoxGeometry(1, 1, 1)
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
+console.log(geometry.attributes.uv)
+const material = new THREE.MeshBasicMaterial({
+    map: colorTexture
+ })
+
+
 const mesh = new THREE.Mesh(geometry, material)
 scene.add(mesh)
 
